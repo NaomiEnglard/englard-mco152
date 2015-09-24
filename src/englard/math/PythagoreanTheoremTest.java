b158b242755a2fe8b58c7d5494cf97cf08b5c833
unknown <student1@FB-W216-W8.academic.touro.edu>
2015-09-24 19:37:28 -0400

    fix test to include execptions

--- src/englard/math/PythagoreanTheoremTest.java
+++ src/englard/math/PythagoreanTheoremTest.java
@@ -13,7 +13,7 @@ public class PythagoreanTheoremTest {
 	}
 
 	@Test
-	public void TestSetAC() throws InvalidDataException {
+	public void testSetAC() throws InvalidDataException {
 		PythagoreanTheorem pT = new PythagoreanTheorem();
 		pT.setAC(3.2, 7.3);
 		Assert.assertEquals(6.56, pT.getB(), .01);
@@ -21,10 +21,44 @@ public class PythagoreanTheoremTest {
 	}
 
 	@Test
-	public void TestSetBC() throws InvalidDataException {
+	public void testSetBC() throws InvalidDataException {
 		PythagoreanTheorem pT = new PythagoreanTheorem();
 		pT.setBC(5.0, 10.2);
 		Assert.assertEquals(8.89, pT.getA(), .01);
 
 	}
+
+	@Test
+	public void testGetA() {
+		PythagoreanTheorem pT = new PythagoreanTheorem();
+		try {
+			pT.getA();
+			Assert.fail("no getters before setters");
+		} catch (InvalidDataException e) {
+			// expected
+		}
+	}
+
+	@Test
+	public void testGetB() {
+		PythagoreanTheorem pT = new PythagoreanTheorem();
+		try {
+			pT.getB();
+			Assert.fail("no getters before setters");
+		} catch (InvalidDataException e) {
+			// expected
+		}
+	}
+
+	@Test
+	public void testGetC() {
+		PythagoreanTheorem pT = new PythagoreanTheorem();
+		try {
+			pT.getA();
+			Assert.fail("no getters before setters");
+		} catch (InvalidDataException e) {
+			// expected
+		}
+	}
+
 }
--- src/englard/math/QuadraticEquationTest.java
+++ src/englard/math/QuadraticEquationTest.java
@@ -17,4 +17,25 @@ public class QuadraticEquationTest {
 		Assert.assertEquals(-1.0, q.getNegativeX(), 0.1);
 	}
 
+	@Test
+	public void testThrowsInvalidInputExceptionForA() {
+		QuadraticEquation q;
+		try {
+			q = new QuadraticEquation(0, -3.0, -4.0);
+			Assert.fail("quadratic may not have a 0 as A value");
+		} catch (InvalidInputException e) {
+			// expected
+		}
+	}
+
+	@Test
+	public void testThrowsInvalidInputExceptionForB() {
+		QuadraticEquation q;
+		try {
+			q = new QuadraticEquation(1.0, 0, -4.0);
+			Assert.fail("quadratic may not have a 0 as B value");
+		} catch (InvalidInputException e) {
+			// expected
+		}
+	}
 }
