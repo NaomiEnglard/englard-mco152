4ac4677722cd7530f95660f9c9da08c0cee3f859
Naami Englard <Naamibe@gmail.com>
2015-10-16 15:14:09 -0700

    Gui Prodjectile

---
+++ src/englard/prodjectile/ProdjectileGuiMain.java
@@ -0,0 +1,10 @@
+package englard.prodjectile;
+
+import javax.swing.JFrame;
+
+public class ProdjectileGuiMain extends JFrame{
+	public static void main(String[] args){
+	ProdjectileGui frame = new ProdjectileGui();
+	frame.setVisible(true);
+	}
+}
