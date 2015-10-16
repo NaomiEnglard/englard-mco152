1dab2cea9922a87a77605496fc1edcd2ae570ab1
Naami Englard <Naamibe@gmail.com>
2015-10-16 00:31:07 -0700

    Finish quadratic gui, start prodjectile gui

---
+++ src/englard/prodjectile/ProdjectileGui.java
@@ -0,0 +1,27 @@
+package englard.prodjectile;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class ProdjectileGui extends JFrame{
+	private JButton button1;
+	private JButton button2;
+	private JButton button3;
+	private JTextField textField1;
+	private JTextField textField2;
+	private JTextField textField3;
+	private JButton enter;
+	private JLabel output;	
+
+	
+	public ProdjectileGui() {
+		setTitle("ProdjectileCalculator");
+		setSize(400,400);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		
+	}
+
+}
