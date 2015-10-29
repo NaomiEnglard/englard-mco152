baf4edf23a21efd7018dc2c86e97b55ae1f9f8d6
unknown <student1@FB-W216-W8.academic.touro.edu>
2015-10-29 19:37:47 -0400

    eye ball move with mouse gui

--- src/englard/gui/SmileComponent.java
+++ src/englard/gui/SmileComponent.java
@@ -14,7 +14,7 @@ public class SmileComponent extends JComponent {
 	public Point mousePostion;
 
 	public SmileComponent() {
-		//this.y = 50;
+		// this.y = 50;
 	}
 
 	/**
@@ -32,40 +32,28 @@ public class SmileComponent extends JComponent {
 		g.drawArc(90, 100, 55, 100, 45, 100);
 		g.drawArc(210, 100, 55, 100, 45, 100);
 		// g.drawArc(x, y, width, height, startAngle, arcAngle);
-		
-		
-		g.setColor(Color.GREEN);
-		
-		/*if (y <= 50 && y > 0) {
-			g.fillOval(90, 135, 50, y--);
-		}
-		if (y <= 0) {
-			y = 50;
-		}
-		try {
-			Thread.sleep(16);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		super.repaint();
-		*/
 
-		mousePostion =MouseInfo.getPointerInfo().getLocation();
-		x =mousePostion.x;
-		y= mousePostion.y;
-		
+		g.setColor(Color.GREEN);
+
+		/*
+		 * if (y <= 50 && y > 0) { g.fillOval(90, 135, 50, y--); } if (y <= 0) {
+		 * y = 50; } try { Thread.sleep(16); } catch (InterruptedException e) {
+		 * // TODO Auto-generated catch block e.printStackTrace(); }
+		 * super.repaint();
+		 */
+
+		mousePostion = MouseInfo.getPointerInfo().getLocation();
+		x = mousePostion.x;
+		y = mousePostion.y;
+
 		g.fillOval(210, 135, 60, 60);
 		g.fillOval(90, 135, 60, 60);
 		g.setColor(Color.WHITE);
-		
-		
-		
-		g.fillOval(x/30+90, y/30+150, 12, 12);
-		g.fillOval(x/30+210, y/30+150, 12, 12);
-		
+
+		g.fillOval(x / 30 + 90, y / 30 + 150, 12, 12);
+		g.fillOval(x / 30 + 210, y / 30 + 150, 12, 12);
+
 		super.repaint();
-		
 
 		g.setColor(Color.RED);
 		g.drawArc(110, 175, 150, 120, 180, 180);
