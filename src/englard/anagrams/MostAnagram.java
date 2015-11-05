d728bfb3a8696cc5d13e21b248d0ef46d8a9fe16
Naami Englard <Naamibe@gmail.com>
2015-11-04 19:09:15 -0800

    change anagrams to have arrayList of words

--- src/englard/anagrams/MostAnagram.java
+++ src/englard/anagrams/MostAnagram.java
@@ -1,78 +1,82 @@
 package englard.anagrams;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map.Entry;
-import java.util.Scanner;
+import java.util.Map;
 
 public class MostAnagram {
 
 	public static void main(String[] args) {
-		HashMap<String, Integer> letter = new HashMap<String, Integer>();
+		HashMap<String, ArrayList<String>> letter = new HashMap<String, ArrayList<String>>();
 		String most = null;
 		String otherMost = null;
 		int biggest = 0, value;
-		ArrayList<SortedAndUnsotedWord> dicWords = new ArrayList<SortedAndUnsotedWord>();
-		Scanner scanner = null;
+		ArrayList<String> dicWords = new ArrayList<String>();
+		BufferedReader read = null;
+		String line = null;
 		try {
-			scanner = new Scanner(new File("./us.dic"));
+			read = new BufferedReader(new FileReader(("./us.dic")));
+			line = read.readLine();
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			System.out.println("file not found");
+		} catch (IOException e) {
+			System.out.println("problem reading in data");
 		}
-		while (scanner.hasNext()) {
-			String unsortedWord = scanner.next().toLowerCase();
+		while (line != null) {
+			String unsortedWord = line.toLowerCase();
+			dicWords.add(line); // word list
 			char[] letters = unsortedWord.toCharArray();
 			Arrays.sort(letters);
-			String word = new String(letters);
-			dicWords.add(new SortedAndUnsotedWord(unsortedWord, word)); // keep
-																		// a
-																		// list
-																		// of
-																		// all
-																		// the
-																		// words
-			if (letter.get(word) == null) { // if no word with same letters add
-											// new hashmap
-				letter.put(word, 1);
+			String sortedLetters = new String(letters);
+			if (letter.get(sortedLetters) == null) { // if no word with same
+														// letters add
+				// new hashmap
+				ArrayList<String> words = new ArrayList<String>();
+				words.add(line);
+				letter.put(sortedLetters, words);
 			} else { // if there is already a word with these letters increment
 						// hashmap
-
-				letter.put(word, letter.get(word) + 1);
+				ArrayList<String> words = letter.get(sortedLetters);
+				words.add(line);
+				letter.replace(sortedLetters, words);
 			}
+
 			// update the bigest value counter
-			value = letter.get(word);
+			value = letter.get(sortedLetters).size();
 			if (value == biggest) {
 				// hold this map also
-				otherMost = word;
+				otherMost = sortedLetters;
 			}
 			if (value > biggest) {
 				biggest = value;
 				// keep the letters from the bigesst search
-				most = word;
+				most = sortedLetters;
 			}
-
+			// reset line
+			try {
+				line = read.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 
 		// display results
-		int counterOne =0;
-		int counterTwo =0;
-		System.out.printf("The biggest anagram is %s word  \n", biggest);
-		for (SortedAndUnsotedWord word : dicWords) {
-			if (word.getSortedWord().equals(most)) {
-				System.out.print(++counterOne);
-				System.out.println(word.getWord());
-			} else if (word.getSortedWord().equals(otherMost)) {
-				if(counterTwo ==0){
-					System.out.printf("\n Another anagram with %s word \n", biggest);
-				}
-				System.out.print(++counterTwo);
-				System.out.println(word.getWord());
+
+		System.out.printf("The biggest anagrams are %s word  \n", biggest);
+		for (Map.Entry<String, ArrayList<String>> mapValue : letter.entrySet()) {
+			if (mapValue.getKey().equals(most)) {
+				System.out.print(mapValue.getValue());
+			}
+			if (mapValue.getKey().equals(otherMost)) {
+				System.out.println(mapValue.getValue());
 			}
 		}
-		
 
 	}
 }
